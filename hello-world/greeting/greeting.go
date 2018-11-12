82b7f022cb62af97291971e4bf6ac071efc86477
Jimmy Y. Huang <jimmy.huang@duragility.com>
2018-11-12 00:49:44 -0800

    first commit

---
+++ hello-world/greeting/greeting.go
@@ -0,0 +1,6 @@
+package greeting
+
+// The classical introductory exercise. Just say "Hello, World!".
+func HelloWorld() string {
+	return "Hello, World!"
+}
---
+++ hello-world/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"hello-world/greeting"
+)
+
+func main() {
+	fmt.Println(greeting.HelloWorld())
+}
